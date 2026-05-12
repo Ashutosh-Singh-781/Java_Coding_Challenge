45ec6b27543a869803acaa2828053a5d23ca11b6
ASHUTOSH KUMAR SINGH <ashutoshkumarsingh781@gmail.com>
2026-05-12 22:34:41 +0530

    Add files via upload

---
+++ DAY_03/swaptwonumber.java
@@ -0,0 +1,15 @@
+public class swaptwonumber {
+    public static void main(String[] args){
+        int a=10;
+        int b=30;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("now a is "+a);
+        System.out.println("now b is "+b);
+
+
+    }
+    
+}
