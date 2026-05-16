d1e786a6a7e3d8fc837d9e47e7d2a8c975b9ca6a
ASHUTOSH KUMAR SINGH <ashutoshkumarsingh781@gmail.com>
2026-05-16 22:46:45 +0530

    Add files via upload

---
+++ DAY_06/reactangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class reactangle {
+    public static void main(String[] args) {
+        
+        int result;
+        int l;
+        int b;
+        Scanner sc=new Scanner(System.in);
+
+
+        l=sc.nextInt();
+        b=sc.nextInt();
+        result=l*b;
+        System.out.println(result);
+
+
+
+    }
+    
+    
+    
+}
